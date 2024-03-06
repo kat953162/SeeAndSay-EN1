506eac0373acaf2e68e5983566e1a9702cfb6076
Atlai Esteban <ur1124787@email.ccbcmd.edu>
2024-03-06 17:50:48 -0500

    Imported Rex

--- AnimalImpl/ur1124787.java
+++ AnimalImpl/ur1124787.java
@@ -13,7 +13,7 @@ public class ur1124787 extends Animal implements Talker {
     public String getAnimalName() {return "T-Rex";}
 
     @Override
-    public String getSound() {return "RAWR X3 TWEET-TWEETTTT~~";}
+    public String getSound() {return "I swing my small arms in the wind, I chomp on my prey and tweet like bird xd";}
 
     @Override
     public  String getDescription() {return "Friendly Rex";}
